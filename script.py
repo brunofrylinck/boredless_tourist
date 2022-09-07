7481a2573c65ab01fe1887712942f0c34755eb4e
brunofrylinck <brunofrylinck@gmail.com>
2022-09-07 13:37:20 +0200

    Created attractions and functionality for adding new attractions

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 
-destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo Egypt"]
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical sites", "art"]]
 
 # function to return the index of the destination
@@ -17,3 +17,29 @@ def get_traveler_location(traveler):
 
 test_destination_index = get_traveler_location(test_traveler)
 print(test_destination_index)
+
+# define empty list of attractions
+# attractions = []
+# for destination in destinations:
+#     attractions.append([])
+attractions = [[] for destination in destinations]
+print(attractions)
+
+# return a list of attractions based on the destination and input attraction
+def add_attraction(destination, attraction):
+    destination_index = get_destination_index(destination)
+    attractions_for_destination = attractions[destination_index]
+    attractions_for_destination.append(attraction)
+    return attractions_for_destination
+
+add_attraction("Los Angeles, USA", ["Venice Beach", ["beach"]])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
